bd5fbc9f659d348322ea413ea0b8c90238e32ab8
Antonio <33932744+antor44@users.noreply.github.com>
2022-10-03 04:04:35 +0200

    IVSHMEM mode version

---
+++ virt_sound.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# virt-manager and scream launch, IVSHMEM mode
+# scream and bash script in the same directory
+
+virt-manager
+PIDX=$(ps k -start_time exo pid,comm,tty | grep ' '"virt-manager"' ') # $! doesn't work
+PIDX=${PIDX%%virt-manager*}
+echo $PIDX
+PIDZ=0
+
+while [ $? -eq 0 ]; do
+	sleep 5
+	if [ -f /dev/shm/scream-ivshmem ] && [ $PIDZ -eq 0 ]; then
+		./scream -m /dev/shm/scream-ivshmem& # for IVSHMEM the device must exist 
+		PIDZ=$!
+		echo $PIDZ
+	else
+		ps $PIDX 1>/dev/null
+		if [ $? -ne 0 ]; then
+			if  [ $PIDZ -ne 0 ]; then
+				kill $PIDZ 2>/dev/null
+			fi
+			exit 0
+		fi
+	fi
+done
